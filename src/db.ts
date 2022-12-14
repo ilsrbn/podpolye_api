cad75231ddae3a180120f0fc96005cfbc19cb0a5
illia.serbin <illia.serbin@tangramltd.com>
2022-12-14 17:42:28 +0200

    fixed creds issue

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,17 @@
-import { DataSource } from "typeorm";
+import {DataSource, DataSourceOptions} from "typeorm";
 import 'reflect-metadata'
 import dotenv from 'dotenv'
 dotenv.config()
 const env = process.env
-const port = parseInt(env.PORT || '3307')
+const port = parseInt(env.DB_PORT || '3307')
+const db = "mysql";
 export default new DataSource({
-  type: 'mysql',
-  host: '172.18.0.1',
-  port: 3307,
-  username: 'padmin',
-  password: 'aloha728',
-  database: "podpolye",
+  type: db,
+  host: env.DB_HOST,
+  port: port,
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
   entities: ["dist/resources/*/*.model.js"],
   driver: {},
   // logging: true,
