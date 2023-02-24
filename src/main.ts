79de00f446e54aff80fa33f3766e58d324b9846d
illia.serbin <illia.serbin@tangramltd.com>
2023-02-24 19:19:18 +0200

    custom port

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/documentation', app, document);
-  await app.listen(3000);
+  await app.listen(3005);
 }
 bootstrap();
